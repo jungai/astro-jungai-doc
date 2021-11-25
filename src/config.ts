1faeb3bf7382e67485a4821b42187335252e1439
jungai <juworaphol125@gmail.com>
2021-11-26 01:20:42 +0700

    add config file

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,13 @@
 export interface Sidebar {
   text: string;
   header: boolean;
+  link?: string;
 }
 
 export const sideBar: Sidebar[] = [
   { text: 'Installation', header: true },
-  { text: 'zsh', header: false },
-  { text: 'fish', header: false },
-  { text: 'vscode', header: false },
-  { text: 'vim', header: false },
+  { text: 'zsh', header: false, link: '/guides/zsh' },
+  { text: 'fish', header: false, link: '/guides/fish' },
+  { text: 'vscode', header: false, link: '/guides/vscode' },
+  { text: 'vim', header: false, link: '/guides/vim' },
 ];
